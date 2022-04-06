f48dca714bf11faaa5fb458efc3b1de70ebb87bc
larsomic <michael.j.larson@wsu.edu>
2022-04-05 19:21:42 -0700

    Adding function call on click of menu dropdown

--- personalWebsite/frontend/src/components/App.js
+++ personalWebsite/frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import Header from "./Header.js"
 
 export default class App extends Component {
     constructor(props){
@@ -8,9 +9,7 @@ export default class App extends Component {
 
     render() {
         return(
-            <div>
-              Hello world
-            </div>
+              <Header/>
         )
     }
 }
--- personalWebsite/frontend/urls.py
+++ personalWebsite/frontend/urls.py
@@ -5,7 +5,4 @@ app_name = 'frontend'
 
 urlpatterns = [
     path('', index, name=''),
-    path('join', index),
-    path('create', index),
-    path('room/<str:roomCode>', index),
 ]
